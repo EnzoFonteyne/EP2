aba1ae711851d5c8b1bd0b05a3bbc9c766a44832
Enzo Fonteyne <enzof@al.insper.edu.br>
2021-04-27 17:45:39 -0300

    cores diferentes para naipe, bug none ainda existe

--- display.py
+++ display.py
@@ -1,11 +1,21 @@
 from cria_baralho import cria_baralho
+from extrai_naipe import extrai_naipe
 
 baralho = cria_baralho()
 
 def display(baralho):
     n=1
     for cartas in baralho:
-        print('{0}. {1}' .format(n, cartas))
+        naipe = extrai_naipe(cartas)
+        if naipe == '♠':
+            print('\033[37m {0}. \033[36m{1}' .format(n, cartas))
+        elif naipe == '♥':
+            print('\033[37m {0}. \033[31m{1}' .format(n, cartas))
+        elif naipe == '♦':
+            print('\033[37m {0}. \033[32m{1}' .format(n, cartas))
+        else:
+            print('\033[37m {0}. \033[35m{1}' .format(n, cartas))
+        
         n+=1
     return
 
