26c7b669a9da84e2199ee5c538e55a95124a87f8
Enzo Fonteyne <enzof@al.insper.edu.br>
2021-04-27 14:02:44 -0300

    Possui movimentos possiveis

---
+++ possui_movimentos.py
@@ -0,0 +1,27 @@
+def possui_movimentos_possiveis(baralho):
+    for i in range (len(baralho)):
+        carta = baralho[i]
+        valor = extrai_valor(carta)
+        naipe = extrai_naipe(carta)
+        if i > 2:
+            carta3 = baralho[i-3]
+            valor3 = extrai_valor(carta3)
+            naipe3 = extrai_naipe(carta3)
+            if valor == valor3 or naipe == naipe3:
+                resultado = True
+            
+            carta1 = baralho[i-1]
+            valor1 = extrai_valor(carta1)
+            naipe1 = extrai_naipe(carta1)
+            if valor == valor1 or naipe == naipe1:
+                resultado = True
+        elif i > 0:
+            carta1 = baralho[i-1]
+            valor1 = extrai_valor(carta1)
+            naipe1 = extrai_naipe(carta1)
+            if valor == valor1 or naipe == naipe1:
+                resultado = True 
+        else:
+            resultado = False      
+    
+    return resultado
