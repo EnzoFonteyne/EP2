2ad66f290d1f439c55dbacee93e78275c8f9b797
Enzo Fonteyne <enzof@al.insper.edu.br>
2021-04-28 14:20:28 -0300

    Aprimorando o display, erro none ainda existe

--- display.py
+++ display.py
@@ -5,18 +5,17 @@ baralho = cria_baralho()
 
 def display(baralho):
     n=1
+    print('Essa são as suas cartas:')
     for cartas in baralho:
         naipe = extrai_naipe(cartas)
         if naipe == '♠':
-            print('\033[37m{0}. \033[96m{1}' .format(n, cartas))
+            print('\033[37m{0}. \033[96m{1}\033[0m' .format(n, cartas))
         elif naipe == '♥':
-            print('\033[37m{0}. \033[91m{1}' .format(n, cartas))
+            print('\033[37m{0}. \033[91m{1}\033[0m' .format(n, cartas))
         elif naipe == '♦':
-            print('\033[37m{0}. \033[92m{1}' .format(n, cartas))
+            print('\033[37m{0}. \033[92m{1}\033[0m' .format(n, cartas))
         else:
-            print('\033[37m{0}. \033[95m{1}' .format(n, cartas))
+            print('\033[37m{0}. \033[95m{1}\033[0m' .format(n, cartas))
         
         n+=1
-    return
-
 print(display(baralho))
