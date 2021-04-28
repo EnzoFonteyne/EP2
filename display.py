387298d630bc027a184454f53da7fccd4843ba22
Enzo Fonteyne <enzof@al.insper.edu.br>
2021-04-28 13:40:25 -0300

    Removendo o espaço no incio, none erro

--- display.py
+++ display.py
@@ -8,13 +8,13 @@ def display(baralho):
     for cartas in baralho:
         naipe = extrai_naipe(cartas)
         if naipe == '♠':
-            print('\033[37m {0}. \033[36m{1}' .format(n, cartas))
+            print('\033[37m{0}. \033[36m{1}' .format(n, cartas))
         elif naipe == '♥':
-            print('\033[37m {0}. \033[31m{1}' .format(n, cartas))
+            print('\033[37m{0}. \033[31m{1}' .format(n, cartas))
         elif naipe == '♦':
-            print('\033[37m {0}. \033[32m{1}' .format(n, cartas))
+            print('\033[37m{0}. \033[32m{1}' .format(n, cartas))
         else:
-            print('\033[37m {0}. \033[35m{1}' .format(n, cartas))
+            print('\033[37m{0}. \033[35m{1}' .format(n, cartas))
         
         n+=1
     return
