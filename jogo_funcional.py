5cd7a73afc02a89dca6ee7c6158cddad99238957
Enzo Fonteyne <enzof@al.insper.edu.br>
2021-05-03 17:04:56 -0300

    Colocando o display

--- jogo_funcional.py
+++ jogo_funcional.py
@@ -4,14 +4,16 @@ from extrai_naipe import extrai_naipe
 from extrai_valor import extrai_valor
 from possui_movimentos import possui_movimentos_possiveis
 from empilha import empilha
+from display import display
 
 
 baralho = cria_baralho()
 
 movimentos = True
 while movimentos:
+    print(display(baralho))
     movimentos = possui_movimentos_possiveis(baralho)
-    posicao = int(input('Escolha uma numero de 1 a {0}'.format(len(baralho)))
+    posicao = int(input('Escolha uma numero de 1 a {0}: '.format(len(baralho))))
     possivel = lista_movimentos_possiveis(baralho, posicao)
     if len(possivel) < 1:
         print('Essa carta não possui movimentos')
