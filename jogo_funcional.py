70d5ea7b4eb29446d6e26b616787121737c0f467
franciscosilva03 <franciscolps1@al.insper.edu.br>
2021-05-03 17:46:27 -0300

    jogo

--- jogo_funcional.py
+++ jogo_funcional.py
@@ -15,14 +15,16 @@ while movimentos:
     movimentos = possui_movimentos_possiveis(baralho)
     posicao = int(input('Escolha uma numero de 1 a {0}: '.format(len(baralho))))
     possivel = lista_movimentos_possiveis(baralho, posicao)
-    if len(possivel) < 1:
+    while len(possivel) < 1:
         print('Essa carta não possui movimentos')
-    else:
+        posicao = int(input('Escolha uma numero de 1 a {0}: '.format(len(baralho))))
+        possivel = lista_movimentos_possiveis(baralho, posicao)
+
         if len(possivel) == 2:
             soma = int(input('Escolha uma posição para mover (1-2): '))
             if soma == 2:
                 soma = 3
         else:
-            possivel[0] = soma
+            soma = possivel[0]
         destino = posicao - soma
         baralho = empilha(baralho, posicao, destino)
